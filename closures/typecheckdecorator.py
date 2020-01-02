4968d77c6ac6474aab830b675433d9d5b200deaa
nehavari <vari.neha@gmail.com>
2020-01-02 20:37:02 +0530

    Improving names in type check decorator

--- closures/typecheckdecorator.py
+++ closures/typecheckdecorator.py
@@ -4,39 +4,40 @@ Note that this copies the func_name attribute from the old to the new function.
 __name__ was made writable in Python 2.4a3
 '''
 
+import functools
 
 def accepts(*types):
     print("* Inside accepts *")
 
-    def check_accepts(f):
+    def _decorator(f):
         print("** Inside check_accepts **")
         # assert len(types) == f.__code__.co_argcount
+        functools.wraps(f)  # automatic name
 
-        def new_f(*args, **kwds):  # This is the wrapper on decorated function "func"
-            print("*** Inside accepts new_f ***")
+        def _wrapper(*args, **kwds):  # This is the wrapper on decorated function "func"
+            print("*** Inside accepts _wrapper ***")
             for (a, t) in zip(args, types):
                 assert isinstance(a, t), "arg %r does not match %s" % (a, t)
             return f(*args, **kwds)
-        new_f.__name__ = f.__name__
-        return new_f
-    return check_accepts
+        return _wrapper
+    return _decorator
 
 
 def returns(rtype):
     print("* Inside Returns *")
 
-    def check_returns(f):
+    def _decorator(f):
         print("** Inside check_returns **")
 
-        def new_f(*args, **kwds):  # This is the wrapper on decorated function "func"
-            print("*** Inside returns new_f ***")
+        def _wrapper(*args, **kwds):  # This is the wrapper on decorated function "func"
+            print("*** Inside returns _wrapper ***")
             result = f(*args, **kwds)  # This will Not Execute the function, function will be execute when user will
             # call decorated function func() from main() method
             assert isinstance(result, rtype), "return value %r does not match %s" % (result, rtype)
             return result
-        new_f.__name__ = f.__name__
-        return new_f
-    return check_returns
+        _wrapper.__name__ = f.__name__
+        return _wrapper
+    return _decorator
 
 
 @accepts(int, (int, float))
